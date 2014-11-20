86747d78cb67aedc9985afabd561574c44247dd6
Lex Knape <LexKnape@users.noreply.github.com>
2014-11-20 14:06:14 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function is part of the Coursera cdata science course
+## In order to speed up computation it is better to cache the inverse of a matrix 
+## instead of computing it agaian and again. 
 
-## Write a short comment describing this function
+## These (2) functions will be used to cache the inverse of the matrix  
+## The first function:makeCacheMatrix, will create a list of functions with as content: 1. set the value of the matrix
+## 2. get the value of the matrix, 3. set the value of inverse of the matrix, 4. get the value of inverse of the matrix
+## The second function: cacheSolve, assumes that the matrix is always invertible
+
+## The first function: makeCacheMatrix
 
 makeCacheMatrix <- function(x = matrix()) {
 m <- NULL
@@ -21,7 +27,7 @@ getmatrix=getmatrix)
 
 
 
-## Write a short comment describing this function
+## The second function: cacheSolve.
 
 cacheSolve <- function(x=matrix(), ...) {
 m<-x$getmatrix()
